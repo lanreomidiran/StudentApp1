53a127d5cd90a1cfea3bc5390494b32c243a4d7f
Lanre Omidiran <lanreomidiran@gmail.com>
2026-02-26 08:55:50 +0100

    student views

--- StudentApp1/Controllers/StudentsController.cs
+++ StudentApp1/Controllers/StudentsController.cs
@@ -1,37 +1,99 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using StudentApp1.Data;
 using StudentApp1.Models;
 using StudentApp1.Models.Entities;
+using System.ComponentModel.DataAnnotations;
 
-namespace StudentApp1.Controllers;
-
-
-public class StudentsController : Controller
+namespace StudentApp1.Controllers
 {
-    private readonly ApplicationDbContext _dbContext;
-    public StudentsController(ApplicationDbContext dbContext)
+    public class StudentsController : Controller
     {
-        _dbContext = dbContext; 
-    }
+        private readonly ApplicationDbContext dbContext;
 
-    [HttpGet]
-    public IActionResult Add()
-    {
-        return View();
-    }
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
 
-    [HttpPost]
-    public async Task<IActionResult> Add(AddStudentViewModel viewModel);
-    [HttpGet]
-    public async Task<IActionResult> List()
-    {
-        var students = await _dbContext.Students.ToListAsync();
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
 
-        return View(students);
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+           var students = await dbContext.Students.ToListAsync();
+            
+            return View(students);
+
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id==viewModel.Id);
+            
+            if (student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+
+        }
+
     }
-       
- }
+}
 
 
 
